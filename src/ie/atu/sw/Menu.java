9d67945c61fba3f9c437b51fe18bdaea72d8f35f
Jamie Buick <jamiebuick28@gmail.com>
2025-05-05 18:46:08 +0100

    Added menu class and followed example from lectures to make up the menu working with a case statement

---
+++ src/ie/atu/sw/Menu.java
@@ -0,0 +1,137 @@
+package ie.atu.sw;
+
+import java.util.Scanner;
+import static java.lang.System.out;
+
+public class Menu {
+	private Scanner s;
+	private boolean keepRunning = true;
+
+	public Menu() {
+		s = new Scanner(System.in);
+	}
+
+	public void start() throws Exception {
+		while (keepRunning) {
+			showOptions();
+
+			int userInput = Integer.parseInt(s.next());
+			
+			switch(userInput)
+			{
+				case 1 -> mapFile();
+				case 2 -> textFile();
+				case 3 -> outputFile();
+				case 4 -> configureOptions();
+				case 5 -> encode();
+				case 6 -> decode();
+				case 7 -> extras();
+				case 8 -> keepRunning = false;
+				default -> out.println("[Error] Invalid Selection");
+			}
+		}
+	}
+
+	private void showOptions() throws Exception {
+		// You should put the following code into a menu or Menu class
+		System.out.println(ConsoleColour.WHITE);
+		System.out.println("************************************************************");
+		System.out.println("*     ATU - Dept. of Computer Science & Applied Physics    *");
+		System.out.println("*                                                          *");
+		System.out.println("*              Encoding Words with Suffixes                *");
+		System.out.println("*                                                          *");
+		System.out.println("************************************************************");
+		System.out.println("(1) Specify Mapping File");
+		System.out.println("(2) Specify Text File to Encode");
+		System.out.println("(3) Specify Output File (default: ./out.txt)");
+		System.out.println("(4) Configure Options");
+		System.out.println("(5) Encode Text File");
+		System.out.println("(6) Decode Text File");
+		System.out.println("(?) Optional Extras...");
+		System.out.println("(?) Quit");
+
+		// Output a menu of options and solicit text from the user
+		System.out.print(ConsoleColour.BLACK_BOLD_BRIGHT);
+		System.out.print("Select Option [1-?]>");
+		System.out.println();
+
+		
+		/*
+		// You may want to include a progress meter in you assignment!
+		System.out.print(ConsoleColour.YELLOW); // Change the colour of the console text
+		int size = 100; // The size of the meter. 100 equates to 100%
+		for (int i = 0; i < size; i++) { // The loop equates to a sequence of processing steps
+			printProgress(i + 1, size); // After each (some) steps, update the progress meter
+			Thread.sleep(10); // Slows things down so the animation is visible
+		}
+		*/
+	}
+	
+	
+	
+	private void mapFile() {
+		System.out.println("map");
+	}
+	private void textFile() {
+		System.out.println("text");	
+	}
+		
+	private void outputFile() {
+		System.out.println("out");
+	}
+	
+	private void configureOptions() {
+		System.out.println("config");
+	}
+	
+	private void encode() {
+		System.out.println("encode");
+	}
+	
+	private void decode() {
+		System.out.println("decode");
+	}
+	
+	private void extras() {
+		System.out.println("extra");
+	}
+
+
+	
+	
+
+	public static void printProgress(int index, int total) {
+		if (index > total)
+			return; // Out of range
+		int size = 50; // Must be less than console width
+		char done = '█'; // Change to whatever you like.
+		char todo = '░'; // Change to whatever you like.
+
+		// Compute basic metrics for the meter
+		int complete = (100 * index) / total;
+		int completeLen = size * complete / 100;
+
+		/*
+		 * A StringBuilder should be used for string concatenation inside a loop.
+		 * However, as the number of loop iterations is small, using the "+" operator
+		 * may be more efficient as the instructions can be optimized by the compiler.
+		 * Either way, the performance overhead will be marginal.
+		 */
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		for (int i = 0; i < size; i++) {
+			sb.append((i < completeLen) ? done : todo);
+		}
+
+		/*
+		 * The line feed escape character "\r" returns the cursor to the start of the
+		 * current line. Calling print(...) overwrites the existing line and creates the
+		 * illusion of an animation.
+		 */
+		System.out.print("\r" + sb + "] " + complete + "%");
+
+		// Once the meter reaches its max, move to a new line.
+		if (done == total)
+			System.out.println("\n");
+	}
+}
