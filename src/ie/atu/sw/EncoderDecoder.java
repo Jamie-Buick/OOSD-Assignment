9f4582b53b09cae5ce63f090a209c8f285cb45c3
Jamie-Buick <jamiebuick28@gmail.com>
2025-05-12 14:24:45 +0100

    I have the encoder finding the longest prefix in the array based on the word entered

--- src/ie/atu/sw/EncoderDecoder.java
+++ src/ie/atu/sw/EncoderDecoder.java
@@ -2,81 +2,96 @@ package ie.atu.sw;
 
 // Decoding and Encoding class 
 public class EncoderDecoder {
-	
+
 	private static String[] encodedWords = new String[15];
-	
+
 	public static void encode(String s) {
 		int counter = 0;
-		
-	   
+
+		String nextMatch = null;
+		String matchedEncoding = null;
+
 		for(int rows = 0; rows < EncodingFileProcessor.getEncodings().length; rows++) {
 			
+			String match = EncodingFileProcessor.getEncodings()[rows][0];
+			String encoded = EncodingFileProcessor.getEncodings()[rows][1];
+
 			if(EncodingFileProcessor.getEncodings()[rows][0].equals(s)) 
 			{
-				
-				encodedWords[counter] = EncodingFileProcessor.getEncodings()[rows][1];
+				encodedWords[counter] = encoded;
 				//System.out.println(EncodingFileProcessor.encodings[rows][1]);
 				counter++;
+
+				break;
 			}
-			else
+
+			if (s.startsWith(EncodingFileProcessor.getEncodings()[rows][0]))
 			{
-				
+				if(nextMatch == null || match.length() > nextMatch.length())
+				{
+					nextMatch = match;
+					matchedEncoding = encoded;
+
+				}
+
+
+			}
+		}
+
+		if(nextMatch != null)
+		{
+			encodedWords[counter] = matchedEncoding;
+			//System.out.println(EncodingFileProcessor.encodings[rows][1]);
+			counter++;
+		}
+
+
+		/*
+
 				if (EncodingFileProcessor.getEncodings()[rows][0].startsWith("@@"))    // && isStringLengthEqual(EncodingFileProcessor.getEncodings()[rows][0], s) ))
 				{
 					String prefixStrip = EncodingFileProcessor.getEncodings()[rows][0].replace("@@", "");
 
-					if (s.endsWith(prefixStrip)) 
+					if (s.endsWith(prefixStrip))
 					{
 						encodedWords[counter] = EncodingFileProcessor.getEncodings()[rows][1];
 						//System.out.println(EncodingFileProcessor.encodings[rows][1]);
 						counter++;
-					
+
 					}	
-					
+
 				}
-				
-			/*
-				if((EncodingFileProcessor.getEncodings()[rows][0].startsWith("@@")) && (s.endsWith(EncodingFileProcessor.getEncodings()[rows][0]))) //&& EncodingFileProcessor.getEncodings()[rows][0].endsWith("@@" + s)) 
-				{
-					encodedWords[counter] = EncodingFileProcessor.getEncodings()[rows][1];
-					//System.out.println(EncodingFileProcessor.encodings[rows][1]);
-					counter++;
-				}
-			*/
-			}
-		}
-		
-		
-		
-		
+		 */
+
+
+
+
+
 		for (int i = 0; i < counter; i++)
 		{
 			System.out.println(encodedWords[i]);
 		}
-		
+
 		/*
 		for (int cols = 0; cols < 2; cols++) {
-			
+
 			System.out.print(EncodingFileProcessor.encodings[rows][cols] + "		" );
 		}
 		System.out.println("");
-		
+
 		 */		
 		//return "hello";
-	
+
 	}
-	
-	private static boolean isStringLengthEqual(String s, String t) {
-		return s.length() == t.length();
-	}
-	
-	
-	
-	
+
+
+
+
+
 	public String decode(String s) {
 		return null;
-	
+
 	}
-	
-	
+
+
 }
